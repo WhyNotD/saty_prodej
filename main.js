f95d649377d400e8f5bc5f576bd16091b9b1e3d9
Dave Novak <dave.novak11@gmail.com>
2020-03-03 12:05:34 +0100

    added fading

---
+++ main.js
@@ -0,0 +1,12 @@
+/*FADE MODAL*/
+
+$(document).ready(function() {
+    $('a[href="#ex1"]').click(function() {
+        $(this).modal({
+            fadeDuration: 750,
+            fadeDelay: 1.2
+        });
+        return false;
+
+    });
+});
